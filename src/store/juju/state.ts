606d105b096d3c4822433e3db1f5c171fec9428e
Zicklag <zicklag@katharostech.com>
2020-05-24 02:15:50 +0000

    Start Fleshing Out Juju Vuex Store

--- src/store/juju/state.ts
+++ src/store/juju/state.ts
@@ -32,16 +32,49 @@ export type CloudCredential =
   | AzureCloudCredential
   | GcpCloudCredential;
 
+export interface Controller {
+  name: string;
+  cloud: string;
+  region: string;
+}
+
+export interface Cloud {
+  availableRegions: string[];
+}
+
 export interface JujuStateInterface {
+  controllers: Controller[];
+  clouds: { [key: string]: Cloud };
   cloudCredentials: CloudCredential[];
 }
 
 const state: JujuStateInterface = {
+  controllers: [
+    {
+      name: 'my-controller',
+      cloud: 'aws',
+      region: 'us-west'
+    }
+  ],
+  clouds: {
+    aws: {
+      availableRegions: ['us-west', 'us-east', 'asia']
+    },
+    azure: {
+      availableRegions: ['centralus', 'centralasia']
+    }
+  },
   cloudCredentials: [
     {
-      name: 'my-aws-creds-1',
+      name: 'my-aws-creds',
       accessKey: 'no-so-secret-access-key',
       secretKey: 'super-secret-secret-key'
+    },
+    {
+      name: 'my-azure-creds',
+      adAId: 'y5dh1MGzHBv541YA8ONI3zGjTkLpnf46TCNbWDxi',
+      sId: 'iBMlj6jrl93i27NBYrwNvJYXq2UbhQ+VW29TSfKD',
+      adAP: 'ECEfD+mNfq7YWRwTgfW5vwfxYRp+y7O9Yts5'
     }
   ]
 };
