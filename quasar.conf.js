a9b77dd72eb136a49c58eade842b79acf436f704
Zicklag <zicklag@katharostech.com>
2020-07-04 00:43:50 +0000

    Remove Public Path For Move to juju-lens.katharostech.com

--- quasar.conf.js
+++ quasar.conf.js
@@ -79,9 +79,6 @@ module.exports = configure(function(ctx) {
     build: {
       vueRouterMode: 'hash', // available values: 'hash', 'history'
 
-      publicPath: 'juju-lens',
-      forceDevPublicPath: true,
-
       // rtl: false, // https://quasar.dev/options/rtl-support
       // preloadChunks: true,
       // showProgress: false,
